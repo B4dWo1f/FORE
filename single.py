b3b3c21d2ece08a6a1c34a86097f9f2f65d466b4
BadWolf Raven <noel.garcia@openmailbox.org>
2017-09-27 03:51:19 +0200

    prepare data for a single station

---
+++ single.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+"""
+ prepare data for a single station to anticipate the nan's
+"""
+
+from process import parse_csv
+
+fname = '../../ownCloud/5906X.csv'
+
+D,T,W,Wd,B,Bd,P,Pr,Prt,H = parse_csv(fname)
+
+n = 48
+f = open('datos.dat','w')
+for i in range(T.shape[0]-n):
+   if T[i] != T[i]: continue
+   ts = [T[j] for j in range(i+1,i+n+1)]
+   for l in ts:
+      f.write(str(l)+'   ')
+   f.write(str(T[i])+'\n')
+f.close()
