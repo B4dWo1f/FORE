44adb7d253e7e841b86ac90f13790052f83f7edf
Bad Wolf <noel.garcia@openmailbox.org>
2017-10-19 18:59:39 +0200

    added last_mail to gitignore and old cronjob (now in run.sh)

--- run.sh
+++ run.sh
@@ -15,3 +15,8 @@ cat $HOME/CODES/FORE/stations.csv $HOME/CODES/FORE/stations.csv.old | sort | uni
 # Clean
 mv $HOME/CODES/FORE/stations.csv $HOME/CODES/FORE/stations.csv.old
 mv $HOME/CODES/FORE/stations.csv.1 $HOME/CODES/FORE/stations.csv
+
+
+#### Old cron-job
+#31 */2 * * * cp $HOME/CODES/FORE/stations.csv $HOME/CODES/FORE/stations.csv.old && $HOME/CODES/FORE/spider.py > $HOME/spider.log && $HOME/CODES/FORE/process.py 2> /dev/null
+
