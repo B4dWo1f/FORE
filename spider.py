44af94e4c400c442f5ee458b9eee821d11224e0c
Bad Wolf <noel.garcia@openmailbox.org>
2017-11-09 19:37:27 +0100

    Fix HUGE ERROR in spider. Alpha

--- spider.py
+++ spider.py
@@ -19,7 +19,7 @@ LG = logging.getLogger('main')
 logging.basicConfig(level=logging.DEBUG,
                  format='%(asctime)s %(name)s:%(levelname)s - %(message)s',
                  datefmt='%Y/%m/%d-%H:%M:%S',
-                 filename=HOME+'/spider1.log', filemode='w')
+                 filename=HOME+'/spider.log', filemode='w')
 
 
 def make_request(url):
