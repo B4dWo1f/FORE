6d3c82dd9d38c77fd5baeec1e4646560d67d8fb1
BadWolf Raven <noel.garcia@openmailbox.org>
2017-12-03 14:16:44 +0100

    return ordered stations as generator

--- prepare_data.py
+++ prepare_data.py
@@ -29,18 +29,41 @@ def get_stations(fname='stations.csv',url=False):
    if url: return M[['ID','lon','lat','url']]
    else: return M[['ID','lon','lat']]
 
+def get_N_stations(p0,N=5,fname='stations.csv',url=False):
+   """
+     Returns a generator with the stations ordered by distance to p0
+   """
+   ## Find N closest stations
+   stations = get_stations()
+   IDs,dists = [],[]
+   for _, R in stations.iterrows():
+      IDs.append(R['ID'])
+      dists.append(geo.GPSdistance((R['lon'],R['lat']),p0))
+   X = stations['lon'].values
+   Y = stations['lat'].values
+   IDs = np.array(IDs)
+   dists = np.array(dists)
+   inds = np.argsort(dists)
+   for ID in IDs[inds]:
+      yield ID
+   #return IDs[inds[:N]]
 
-def get_data(date,place,prop=None,fol=fol_root,d=1,n=5):
+
+def get_data(date,place,prop=None,fol=fol_root,d=1,n=5,kind='cubic'):
    """
      Returns the closest data for that date and place (Interpolation in space
      and time)
-     d = delta in time
-     n = number of stations to use
+     d: delta in time
+     n: number of stations to use
    """
-   ## Fix props to return
+   ## Fix parameters
+   # properties to return
    if prop==None: props = names[1:]
    elif isinstance(prop,str): props = [prop]
    else: props = list(prop)
+   # interpolation kind
+   if kind == 'linear': interp2d = LinearNDInterpolator
+   else: interp2d = CloughTocher2DInterpolator
    ## Find N closest stations
    stations = get_stations()
    IDs,dists = [],[]
@@ -57,7 +80,7 @@ def get_data(date,place,prop=None,fol=fol_root,d=1,n=5):
    data,i = [],0
    while len(data) < n and i<len(IDs):
       ID = IDs[inds[i]]
-      data_station = get_data_from_station(ID,date,props)
+      data_station = get_data_from_station(ID,date,props,kind=kind)
       aux = []
       for a in data_station:
          if a == a: aux.append(a)
@@ -67,7 +90,6 @@ def get_data(date,place,prop=None,fol=fol_root,d=1,n=5):
       i += 1
    x = [ix[0] for ix in points]
    y = [ix[1] for ix in points]
-   #f = CloughTocher2DInterpolator(points,data)
    interps = []
    for i in range(len(props)):
       data_aux = []
@@ -81,17 +103,23 @@ def get_data(date,place,prop=None,fol=fol_root,d=1,n=5):
    return [float(f(place)) for f in interps]
 
 
-def get_data_from_station(ID,date,prop=None,fol=fol_root,d=2):
+def get_data_from_station(ID,date,prop=None,fol=fol_root,d=2,kind='cubic'):
    """
      Retrieve data (specific, or all of them) from a given station for a
      specific time.
      This function is equivalent to a interpolation in time for a given station
+     ID: ID of the station
+     date: datetime for which to retrieve the data
+     fol: root folder containing all the data
      d: number of hours used for the interpolation
+     prop: properties to return. If None ==> return all the properties
    returning order:
    temperature,wind,wind dir,gust,gust dir,precipitation,pressure,
                                                       pressure trend,humidity
    """
-   if prop==None: props = names[1:]
+   ## Fix parameters
+   # properties to return
+   if prop == None: props = names[1:]
    elif isinstance(prop,str): props = [prop]
    else: props = list(prop)
    d = dt.timedelta(hours=d)
@@ -106,7 +134,7 @@ def get_data_from_station(ID,date,prop=None,fol=fol_root,d=2):
       x = [x.value for x in M.ix[start:end][prop].index]
       y = M.ix[start:end][prop].values
       if len(x) >= 2:
-         f = interp1d(x, y)  #TODO implement "kind"
+         f = interp1d(x, y, kind=kind)
          a = float(f(x0))
       else: a = float('nan')
       aux.append(a)
